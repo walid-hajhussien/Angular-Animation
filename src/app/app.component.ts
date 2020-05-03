c528a361901e990bbfb5bc22e6ed9a61a19eff07
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-05-03 18:25:38 +0100

    (add) animation trigger & state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {state, style, trigger} from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('init', style({'background-color': 'red', transform: 'translateX(0)'})),
+      state('end', style({'background-color': 'blue', transform: 'translateX(100px)'}))
+    ])
+  ]
 })
 export class AppComponent {
   list = ['Milk', 'Sugar', 'Bread'];
+  divState = 'init';
 
-    onAdd(item) {
+  onAdd(item) {
+    if (item.length > 0) {
       this.list.push(item);
     }
+
+  }
 }
