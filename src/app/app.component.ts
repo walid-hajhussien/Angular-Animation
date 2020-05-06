d0537cecc2d3a718f9c87946fb6cd46f8dbb19a8
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-05-06 15:16:56 +0100

    (add) shrank  animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
   templateUrl: './app.component.html',
   animations: [
     trigger('divState', [
-      state('0', style({'background-color': 'red', transform: 'translateX(0)'})),
-      state('1', style({'background-color': 'blue', transform: 'translateX(100px)'})),
+      state('0', style({'background-color': 'red', transform: 'translateX(0) scale(1)'})),
+      state('1', style({'background-color': 'blue', transform: 'translateX(100px) scale(1)'})),
+      state('2', style({'background-color': 'green', transform: ' scale(0.5)'})),
       transition('0 => 1', animate(300)),
       transition('1 => 0', animate(400)),
-      transition('1 => *', animate(400)),
+      transition('* <=> 2', animate(500)),
     ])
   ]
 })
@@ -21,6 +22,11 @@ export class AppComponent {
 
   onAnimate() {
     this.divState = this.divState ? 0 : 1;
+  }
+
+  onShrink() {
+    console.log(this.divState !== 2)
+    this.divState = (this.divState !== 2) ? 2 : 0;
   }
 
   onAdd(item) {
